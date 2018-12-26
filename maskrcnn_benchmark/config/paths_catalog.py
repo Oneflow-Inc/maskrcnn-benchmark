7e31033f7a6cba9bdf57dd7f82143b4962bb4b3e
ScXfjiang <sc.xfjiang@gmail.com>
2018-12-26 22:42:01 +0800

    run mask rcnn benchmark

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -5,8 +5,16 @@ import os
 
 
 class DatasetCatalog(object):
-    DATA_DIR = "datasets"
+    DATA_DIR = "/dataset"
     DATASETS = {
+        "coco_2017_train": {
+            "img_dir": "mscoco_2017/train2017",
+            "ann_file": "mscoco_2017/annotations/instances_train2017.json"
+        },
+        "coco_2017_val": {
+            "img_dir": "mscoco_2017/val2017",
+            "ann_file": "mscoco_2017/annotations/instances_val2017.json"
+        },
         "coco_2014_train": {
             "img_dir": "coco/train2014",
             "ann_file": "coco/annotations/instances_train2014.json"
---
+++ train.sh
@@ -0,0 +1,2 @@
+python ./tools/train_net.py \
+       --config-file "./configs/customized_e2e_mask_rcnn_R_50_C4_1x.yaml"
