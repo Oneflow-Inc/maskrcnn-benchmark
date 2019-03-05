3ab31bf41825308f142a7d708009605297085b6e
ScXfjiang <sc.xfjiang@gmail.com>
2019-03-05 12:00:36 +0800

    all configs in one yaml

--- train.sh
+++ train.sh
@@ -4,8 +4,8 @@ rm log.txt
 # export NGPUS=1
 # python -m torch.distributed.launch --nproc_per_node=$NGPUS \
 #        ./tools/train_net.py\
-#        --config-file "./configs/customized_e2e_mask_rcnn_R_50_FPN_1x.yaml" \
+#        --config-file "./configs/customized_e2e_mask_rcnn_R_50_FPN_1x_all.yaml" \
 #        --skip-test
 python ./tools/train_net.py\
-       --config-file "./configs/customized_e2e_mask_rcnn_R_50_FPN_1x.yaml" \
+       --config-file "./configs/customized_e2e_mask_rcnn_R_50_FPN_1x_all.yaml" \
        --skip-test
