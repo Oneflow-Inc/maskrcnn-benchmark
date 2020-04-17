b7e419c82beb338c41c1533b0e4161f081a70dba
ScXfjiang <sc.xfjiang@gmail.com>
2020-04-17 20:14:01 +0800

    update config

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 ./clear.sh
-CUDA_VISIBLE_DEVICES=1                                                          \
+CUDA_VISIBLE_DEVICES=1                                                         \
 python ./tools/test_net.py                                                     \
-       --config-file "./configs/customized_e2e_mask_rcnn_R_50_FPN_1x.yaml" 							  \
+       --config-file "./configs/customized_e2e_mask_rcnn_R_50_FPN_1x.yaml" 	   \
